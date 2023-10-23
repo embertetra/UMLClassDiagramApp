23b704241fa4c8a7478f87063dbaf7564e45f1fa
trichogaster <dmitic3322rn@raf.rs>
2023-10-23 13:44:27 +0200

    Baza projekta

---
+++ src/main/java/raf/dsw/classycraft/app/gui/swing/view/MyToolBar.java
@@ -0,0 +1,15 @@
+package raf.dsw.classycraft.app.gui.swing.view;
+
+import raf.dsw.classycraft.app.controller.ExitAction;
+
+import javax.swing.*;
+
+public class MyToolBar extends JToolBar {
+    public MyToolBar(){
+        super(HORIZONTAL);
+        setFloatable(false);
+
+        ExitAction ea = new ExitAction();
+        add(ea);
+    }
+}
