1a3762ccaed8d2c999a3b8b4a3c5f8b3f22a58c5
trichogaster <dmitic3322rn@raf.rs>
2023-10-24 18:49:38 +0200

    Prozor AboutUs kompletan

---
+++ src/main/java/raf/dsw/classycraft/app/controller/AbstractClassyAction.java
@@ -0,0 +1,30 @@
+package raf.dsw.classycraft.app.controller;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import java.net.URL;
+
+public abstract class AbstractClassyAction extends AbstractAction {
+
+    public Icon LoadIcon(String fileName){
+
+
+        Icon icon = null;
+        URL ImageURL = getClass().getResource(fileName);
+        if(ImageURL != null)
+        {
+            Image img = new ImageIcon(ImageURL).getImage();
+            Image newImg = img.getScaledInstance(30, 30, Image.SCALE_DEFAULT);
+            icon = new ImageIcon(newImg);
+        }
+        else
+        {
+            System.err.println("File " + "images/exit.png" + " not found");
+        }
+
+        return icon;
+    }
+
+}
---
+++ src/main/java/raf/dsw/classycraft/app/gui/swing/view/AboutUsFrame.java
@@ -0,0 +1,59 @@
+package raf.dsw.classycraft.app.gui.swing.view;
+
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderPane;
+
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+import java.net.URL;
+
+public class AboutUsFrame extends JFrame {
+    //buduca polja za sve komponente view-a na glavnom prozoru
+
+
+    public AboutUsFrame(){
+        Toolkit kit = Toolkit.getDefaultToolkit();
+        Dimension screenSize = kit.getScreenSize();
+        int screenHeight = screenSize.height;
+        int screenWidth = screenSize.width;
+        setSize(screenWidth / 3, screenHeight / 3);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setTitle("About us");
+
+        Icon icon = null;
+        URL ImageURL = getClass().getResource("/images/slikaaboutus.jpg");
+
+        if(ImageURL != null)
+        {
+            Image img = new ImageIcon(ImageURL).getImage();
+            Image newImg = img.getScaledInstance(250, 300, Image.SCALE_DEFAULT);
+            icon = new ImageIcon(newImg);
+        }
+        else
+        {
+            System.err.println("File " + "images/slikaaboutus.jpg" + " not found");
+        }
+
+        JLabel lblSlika = new JLabel();
+        JLabel lblClan1 = new JLabel("Dimitrije Mitic");
+        JLabel lblClan2 = new JLabel("Marta Ljiljak");
+
+        lblSlika.setIcon(icon);
+        lblSlika.setHorizontalAlignment(0);
+        lblClan1.setHorizontalAlignment(0);
+        lblClan2.setHorizontalAlignment(0);
+
+        BorderLayout borderLayout = new BorderLayout();
+        lblClan1.setBorder(new EmptyBorder(new Insets(10,0,10,0)));
+        lblClan2.setBorder(new EmptyBorder(new Insets(10,0,10,0)));
+        setLayout(borderLayout);
+        add(lblSlika, BorderLayout.CENTER);
+        add(lblClan1, BorderLayout.NORTH);
+        add(lblClan2, BorderLayout.SOUTH);
+
+
+    }
+
+}
