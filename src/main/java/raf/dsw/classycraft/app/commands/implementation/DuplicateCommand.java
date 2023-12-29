0b96615ef325361af65b7d6786425d5aac37b82e
embertetra <mljiljak7422rn@raf.rs>
2023-12-29 18:45:58 +0100

    DuplicateCommand

--- src/main/java/raf/dsw/classycraft/app/commands/implementation/DuplicateCommand.java
+++ src/main/java/raf/dsw/classycraft/app/commands/implementation/DuplicateCommand.java
@@ -1,17 +1,34 @@
 package raf.dsw.classycraft.app.commands.implementation;
 
+import raf.dsw.classycraft.app.classyCraftRepository.composite.ClassyNode;
+import raf.dsw.classycraft.app.classyCraftRepository.composite.dijagramElementi.Interclass;
+import raf.dsw.classycraft.app.classyCraftRepository.composite.dijagramElementi.connection.Agregacija;
+import raf.dsw.classycraft.app.classyCraftRepository.composite.dijagramElementi.connection.Generalizacija;
+import raf.dsw.classycraft.app.classyCraftRepository.composite.dijagramElementi.connection.Kompozicija;
+import raf.dsw.classycraft.app.classyCraftRepository.composite.dijagramElementi.connection.Zavisnost;
+import raf.dsw.classycraft.app.classyCraftRepository.composite.dijagramElementi.interclass.EnumM;
 import raf.dsw.classycraft.app.classyCraftRepository.composite.dijagramElementi.interclass.Interfejs;
 import raf.dsw.classycraft.app.classyCraftRepository.composite.dijagramElementi.interclass.Klasa;
 import raf.dsw.classycraft.app.classyCraftRepository.implementation.Dijagram;
 import raf.dsw.classycraft.app.commands.AbstractCommand;
+import raf.dsw.classycraft.app.gui.swing.tree.ClassyTreeImplementation;
 import raf.dsw.classycraft.app.gui.swing.tree.model.ClassyTreeItem;
 import raf.dsw.classycraft.app.gui.swing.view.DijagramView;
 import raf.dsw.classycraft.app.gui.swing.view.MainFrame;
+import raf.dsw.classycraft.app.gui.swing.view.painters.ElementPainter;
 import raf.dsw.classycraft.app.gui.swing.view.painters.InterclassPainter;
+import raf.dsw.classycraft.app.gui.swing.view.painters.connectionPainter.AgregacijaPainter;
+import raf.dsw.classycraft.app.gui.swing.view.painters.connectionPainter.GeneralizacijaPainter;
+import raf.dsw.classycraft.app.gui.swing.view.painters.connectionPainter.KompozicijaPainter;
+import raf.dsw.classycraft.app.gui.swing.view.painters.connectionPainter.ZavisnostPainter;
+import raf.dsw.classycraft.app.gui.swing.view.painters.interclassPainter.EnumPainter;
 import raf.dsw.classycraft.app.gui.swing.view.painters.interclassPainter.InterfejsPainter;
 import raf.dsw.classycraft.app.gui.swing.view.painters.interclassPainter.KlasaPainter;
 
+import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DuplicateCommand extends AbstractCommand {
 
@@ -57,6 +74,66 @@ public class DuplicateCommand extends AbstractCommand {
 
     @Override
     public void undoCommand() {
+        ///odredjivanje dijagrama unutar stabla
+        ClassyTreeItem item = null;
+        ClassyTreeItem selected = MainFrame.getInstance().getPackageView().getClassyTreeItem();
+        for (int i = 0; i < selected.getChildCount(); i++) {
+            ClassyTreeItem c = (ClassyTreeItem) selected.getChildAt(i);
+            ClassyNode cn = c.getClassyNode();
+            if (cn.getName().equals(dijagramView.getClassyNode().getName()))
+                item = c;
+        }
 
+        for(int j=dijagramView.getElementPainterList().size()-1; j>=0; j--){
+            ElementPainter elementPainter = dijagramView.getElementPainterList().get(j);
+            ///brisanje pojedinacne interklase
+            if (elementPainter instanceof InterclassPainter) {
+                Interclass klasaBrisanje = (Interclass) elementPainter.getElement();
+                if (item != null) {
+                    for (int i = 0; i < item.getChildCount(); i++) {
+                        if (((ClassyTreeItem) item.getChildAt(i)).getClassyNode() instanceof Interclass) {
+                            Interclass inter = (Interclass) ((ClassyTreeItem) item.getChildAt(i)).getClassyNode();
+                            if (inter.poredjenje(klasaBrisanje)) {
+                                Dijagram dijagram = (Dijagram) item.getClassyNode();
+                                Interclass interclass = (Interclass) ((ClassyTreeItem) item.getChildAt(i)).getClassyNode();
+                                dijagram.removeChild(interclass);
+                                item.remove(i);
+                                break;
+                            }
+                        }
+                    }
+                }
+                ClassyTreeImplementation classyTreeImplementation = (ClassyTreeImplementation) MainFrame.getInstance().getClassyTree();
+                SwingUtilities.updateComponentTreeUI(classyTreeImplementation.getTreeView());
+                break;
+            }
+        }
+
+        ///sredjivanje modela i paintera
+        java.util.List<ElementPainter> novaPainterLista = new ArrayList<>();
+        List<ClassyNode> novaLista = new ArrayList<>();
+        for(int i=0;i< item.getChildCount();i++){
+            ClassyTreeItem cti = (ClassyTreeItem) item.getChildAt(i);
+            novaLista.add(cti.getClassyNode());
+            if(cti.getClassyNode() instanceof Klasa)
+                novaPainterLista.add(new KlasaPainter((Klasa) cti.getClassyNode()));
+            else if(cti.getClassyNode() instanceof Interfejs)
+                novaPainterLista.add(new InterfejsPainter((Interfejs) cti.getClassyNode()));
+            else if(cti.getClassyNode() instanceof EnumM)
+                novaPainterLista.add(new EnumPainter((EnumM) cti.getClassyNode()));
+            else if(cti.getClassyNode() instanceof Agregacija)
+                novaPainterLista.add(new AgregacijaPainter((Agregacija) cti.getClassyNode()));
+            else if(cti.getClassyNode() instanceof Kompozicija)
+                novaPainterLista.add(new KompozicijaPainter((Kompozicija) cti.getClassyNode()));
+            else if(cti.getClassyNode() instanceof Generalizacija)
+                novaPainterLista.add(new GeneralizacijaPainter((Generalizacija) cti.getClassyNode()));
+            else if(cti.getClassyNode() instanceof Zavisnost)
+                novaPainterLista.add(new ZavisnostPainter((Zavisnost) cti.getClassyNode()));
+        }
+        Dijagram d = (Dijagram) item.getClassyNode();
+        d.setChildren(novaLista);
+        dijagramView.getElementPainterList().clear();
+        dijagramView.setElementPainterList(novaPainterLista);
+        dijagramView.repaint();
     }
 }
